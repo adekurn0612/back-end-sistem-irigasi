2f30fd785ea464efa1a284d2db2eba3559e53277
dory achmad~ <achmad.hudori1191@gmail.com>
2023-08-08 21:54:52 +0700

    fix

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = Express();
 const port = process.env.PORT;
 
 app.use(Express.json());
+app.get("/about", (req, res) => {
+    res.send("This is the about page.");
+  });
 
 app.use(Express.urlencoded({ extended: true }));
 app.use(cors())
